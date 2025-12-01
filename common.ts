03b1477ad01c8b6fc4cf59f6d63a60e7e9e8fd9d
Anton Ödman <anton.odman@gmail.com>
2025-12-01 21:19:06 +0100

    chore: add prettier

--- common.ts
+++ common.ts
@@ -1,12 +1,12 @@
-import fs from 'fs';
+import fs from "fs";
 
 export const run = (runner: (file: string) => void, input: string) => {
-  const file = fs.readFileSync(input, 'utf-8').trim();
+    const file = fs.readFileSync(input, "utf-8").trim();
 
-  const startTime = Date.now();
-  const result = runner(file);
-  const endTime = Date.now();
+    const startTime = Date.now();
+    const result = runner(file);
+    const endTime = Date.now();
 
-  console.log(`Result: ${result}`);
-  console.log(`Time: ${endTime - startTime}ms`);
-}
+    console.log(`Result: ${result}`);
+    console.log(`Time: ${endTime - startTime}ms`);
+};
--- day01/main.ts
+++ day01/main.ts
@@ -1,12 +1,15 @@
 import { run } from "../common.js";
 
-const part1 = (input: string) => { };
+const part1 = (input: string) => {
+    const lines = input.split("\n");
+    console.log(input);
+};
 
-const part2 = (input: string) => { };
+const part2 = (input: string) => {};
 
 const main = () => {
-  run(part1, "example.txt");
-  run(part2, "example.txt");
-}
+    run(part1, "example.txt");
+    run(part2, "example.txt");
+};
 
 main();
